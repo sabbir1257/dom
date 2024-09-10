d4f8cf7deaa54076716b9024f3a2d985efe2efbb
sabbir1257 <masabbir1257@gmail.com>
2024-09-10 15:24:33 +0600

    Create HTML elements using Javascript and appendChild

---
+++ js/styleInJs.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('section');
+for (const section of sections) {
+     section.style.border = '2px solid black';
+     section.style.marginBottom = '5px';
+     section.style.borderRadius = '15px';
+     section.style.paddingLeft = '7px';
+     section.style.backgroundColor = 'lightgray';  
+}
+
+/* 
+const placesContainer = document.getElementById('places-container');
+placesContainer.style.backgroundColor = 'lightblue'; */
+
+const placesContainer = document.getElementById('places-container');
+placesContainer.classList.add('text-center');
+placesContainer.classList.remove('lange-text')
